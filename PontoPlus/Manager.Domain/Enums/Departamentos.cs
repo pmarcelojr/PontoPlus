4a3eb9cd5743b811d7ebd084e499f038690c9c78
pmarcelojr <marcelosantostecnologia@gmail.com>
2021-09-09 18:05:18 -0300

    updating enum with outros

--- PontoPlus/Manager.Domain/Enums/Departamentos.cs
+++ PontoPlus/Manager.Domain/Enums/Departamentos.cs
@@ -2,16 +2,17 @@ namespace PontoPlus.Manager.Domain.Enums
 {
     public enum Departamentos
     {
-        TI = 0,
-        Marketing = 1,
-        Contabilidade = 2,
-        RH = 3,
-        Juridico = 4,
-        Vendas = 5,
-        Manutenção = 6,
-        Financeiro = 7,
-        Projetos = 8,
-        Comercial = 9,
-        Comunicação = 10
+        TI,
+        Marketing,
+        Contabilidade,
+        RH,
+        Juridico,
+        Vendas,
+        Manutenção,
+        Financeiro,
+        Projetos,
+        Comercial,
+        Comunicação,
+        Outros
     }
 }
