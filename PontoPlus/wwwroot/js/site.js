81c457c2bb58ecd8df79506aabdf98b654dbe569
pmarcelojr <marcelosantostecnologia@gmail.com>
2021-09-09 00:21:29 -0300

    centralizing javascript code

--- PontoPlus/wwwroot/js/site.js
+++ PontoPlus/wwwroot/js/site.js
@@ -2,3 +2,20 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+// Time Ponto Eletrônico -- //Views/Home/Index.cshtml
+function myTime() {
+    var d = new Date(), displayDate;
+    if (navigator.userAgent.toLowerCase().indexOf('firefox') > -1) {
+        displayDate = d.toLocaleTimeString('pt-BR');
+    }
+    else {
+        displayDate = d.toLocaleTimeString('pt-BR', { timeZone: 'America/Sao_Paulo' });
+    }
+    document.getElementById("clock").innerHTML = displayDate;
+}
+
+$().ready(function () {
+    myTime();
+    var myVar = setInterval(myTime, 1000);
+});
