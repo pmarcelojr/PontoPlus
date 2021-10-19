d446d6df78ad0f3ffc51cf19bb1f4b1cc1f166ec
pmarcelojr <marcelosantostecnologia@gmail.com>
2021-10-19 17:19:33 -0300

    renamed project folders

--- PontoPlus/PontoPlus.Infra/Data/PontoPlusContext.cs
+++ PontoPlus/PontoPlus.Infra/Data/PontoPlusContext.cs
@@ -14,10 +14,10 @@ namespace PontoPlus.Manager.Infra.Data
         public virtual DbSet<RegistroPonto> RegistroPontos { get; set; }
         public virtual DbSet<UsuarioMensagem> UsuarioMensagens { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseMySql(@"Server=localhost;Database=PontoPlus;Uid=root;Pwd=root;");
-        }
+        // protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        // {
+        //     optionsBuilder.UseMySql(@"Server=localhost;Database=PontoPlus;Uid=root;Pwd=root;");
+        // }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
